2acf1f9690f5664adfbd47ffe62688e0f89a9d3f
davidcoats <david@getsafetycone.com>
2025-01-06 23:53:09 -0800

    Interim commit.

--- source/R5T.L0053/Code/Functionality/IEnumerationOperator.cs
+++ source/R5T.L0053/Code/Functionality/IEnumerationOperator.cs
@@ -9,33 +9,6 @@ namespace R5T.L0053
     public partial interface IEnumerationOperator : IFunctionalityMarker,
         L0066.IEnumerationOperator
     {
-        public Type Get_UnderlyingType(Enum value)
-        {
-            var enumerationType = value.GetType();
-
-            var output = this.Get_UnderlyingType(enumerationType);
-            return output;
-        }
-
-        public Type Get_UnderlyingType<TEnum>()
-            where TEnum : Enum
-        {
-            var enumerationType = typeof(TEnum);
-
-            var output = this.Get_UnderlyingType(enumerationType);
-            return output;
-        }
-
-        public Type Get_UnderlyingType(Type enumerationType)
-        {
-            var underlyingType = Enum.GetUnderlyingType(enumerationType);
-            return underlyingType;
-        }
-
-        //public bool Is<T>(Enum value, Enum flag)
-        //    where T : Enum
-        //{
-
-        //}
+        
     }
 }
